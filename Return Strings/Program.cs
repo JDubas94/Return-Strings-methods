e7fe5f66de76da1bd00fdcd8bad8474ba8a9919e
Julia Dubas <uliadubas94@gmail.com>
2022-08-15 21:09:18 +0300

    Add project files.

---
+++ Return Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Return_Strings
+{
+    class Program
+    {
+        static string GetHello(string name)
+        {
+            return "Hello, " + name + " ,how are you doing today?";
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine(GetHello(name));
+
+            Console.ReadLine();
+
+        }
+    }
+}
